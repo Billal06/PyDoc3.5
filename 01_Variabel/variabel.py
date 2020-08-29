541e33fa2e6e090e12a4db44b6f3c832de15631d
Billal06 <billal.krsz006@gmail.com>
2020-08-29 19:33:07 +0700

    Menambahkan contoh variabel

---
+++ 01_Variabel/variabel.py
@@ -0,0 +1,10 @@
+ini_int = 10
+ini_str = "Indonesia merderka pada tahun 1945"
+ini_bool = True
+ini_bool2 = False
+ini_decimal = 0.1
+
+print (ini_int)
+print (ini_str)
+print (ini_bool)
+print (ini_decimal)
