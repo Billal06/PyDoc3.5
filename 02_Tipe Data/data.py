1747123af8b0cbd20e965da7dca2986e19b353d0
Billal06 <billal.krsz006@gmail.com>
2020-08-29 19:33:25 +0700

    Menambahkan contoh tipe data

---
+++ 02_Tipe Data/data.py
@@ -0,0 +1,11 @@
+data_int = 1945
+data_float = 17.19
+data_string = "Indonesia merdeka pada tahun 1945"
+data_bool = True
+data_complex = complex(8,17)
+
+print (data_int)
+print (data_float)
+print (data_string)
+print (data_bool)
+print (data_complex)
