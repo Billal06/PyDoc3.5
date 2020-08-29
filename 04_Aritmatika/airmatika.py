db829f5b46ff2ed1fe80a11681f12a99044e622d
Billal06 <billal.krsz006@gmail.com>
2020-08-29 19:34:29 +0700

    Menambahkan contoh Aritmatika

---
+++ 04_Aritmatika/airmatika.py
@@ -0,0 +1,26 @@
+b1 = 17
+b2 = 6
+
+# operasi tambah "+"
+tambah = b1 + b2
+print (tambah)
+
+# Operasi pengurangan "-"
+pengurangan = b1 - b2
+print (pengurangan)
+
+# Operasi perkalian "*"
+perkalian = b1 * b2
+print (perkalian)
+
+# Operasi pembagian "/"
+bagi = b1 / b2
+print (bagi)
+
+# Operasi pangkat "**"
+pangkat = b1 ** b2
+print (pangkat)
+
+# Operasi modulus "%"
+modulus = b1
+print (modulus)
