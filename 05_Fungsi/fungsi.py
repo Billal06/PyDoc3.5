cab13fde31c48b7f020398a24e6033f24b91abc1
Billal06 <billal.krsz006@gmail.com>
2020-08-29 19:34:57 +0700

    Menambahkan contoh function

---
+++ 05_Fungsi/fungsi.py
@@ -0,0 +1,10 @@
+# Membuat fungsi
+def merdeka():
+	print ("Indonesia merdeka pada tahun 1945")
+
+def merdeka2(tahun):
+	print ("Indonesia merdeka pada tahun " + tahun)
+
+# Panggil fungsi
+merdeka()
+merdeka2("1945")
